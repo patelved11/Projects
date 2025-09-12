45e71fed304ca20e389a4f45cb989a99ad0c4671
patelved11 <vedpokar00@gmail.com>
2025-09-12 17:57:01 +0530

    Update db3.php

--- expense_tracker/db3.php
+++ expense_tracker/db3.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sql300.infinityfree.com";
-$user = "if0_39648501";          // default user
-$password = "patelved1";          // default is blank in XAMPP
-$dbname = "if0_39648501_expense_tracker";
+$host = "";
+$user = "";          // default user
+$password = "";          // default is blank in XAMPP
+$dbname = "";
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
@@ -11,3 +11,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
