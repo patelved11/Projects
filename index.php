5a97a7598b2f33bc1a9af2b24a7554cb62b6918e
patelved11 <vedpokar00@gmail.com>
2026-06-06 13:34:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 // index.php at the top level
-header("Location: /expensetracker");
-exit();
+//header("Location: /expensetracker");
+//exit();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projects</title>
+    <link rel="stylesheet" href="assignments.css">
+</head>
+<body>
+    <h1>projects</h1>
+    <div class="container">
+        <ul class="links">
+            <li><a href="Assignment1/index.php">Assignment 1</a></li>
+        </ul>
+    </div>
+    
+</body>
+</html>
